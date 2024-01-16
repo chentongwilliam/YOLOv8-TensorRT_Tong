5bbbf0600838ed061935ecd5ae1b4e3982c9c5a2
ChenTong <chentong.william@gmail.com>
2024-01-16 16:28:32 +0100

    add more time counting

--- infer-seg-without-torch.py
+++ infer-seg-without-torch.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import time
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 from config import ALPHA, CLASSES, COLORS, MASK_COLORS
 from models.utils import blob, letterbox, path_to_list, seg_postprocess
@@ -28,12 +29,28 @@ def main(args: argparse.Namespace) -> None:
 
     frame_count = 0
     start_time = time.time()
+    
+    # init times
+    loading_times = []
+    preprocessing_times = []
+    inference_times = []
+    postprocessing_times = []
+    saving_times = []
+    loop_times = []
 
     for image in images:
         loop_start_time = time.time()
         
         save_image = save_path / image.name
+        
+        # load image
+        start_loading_time = time.time()
         bgr = cv2.imread(str(image))
+        end_loading_time = time.time()
+        loading_times.append(end_loading_time - start_loading_time)
+        
+        # preprocessing
+        start_preprocessing_time = time.time()
         draw = bgr.copy()
         bgr, ratio, dwdh = letterbox(bgr, (W, H))
         dw, dh = int(dwdh[0]), int(dwdh[1])
@@ -41,9 +58,17 @@ def main(args: argparse.Namespace) -> None:
         tensor, seg_img = blob(rgb, return_seg=True)
         dwdh = np.array(dwdh * 2, dtype=np.float32)
         tensor = np.ascontiguousarray(tensor)
+        end_preprocessing_time = time.time()
+        preprocessing_times.append(end_preprocessing_time - start_preprocessing_time)
+        
         # inference
+        start_inference_time = time.time()
         data = Engine(tensor)
-
+        end_inference_time = time.time()
+        inference_times.append(end_inference_time - start_inference_time)
+        
+        # postprocessing
+        start_postprocessing_time = time.time()
         seg_img = seg_img[dh:H - dh, dw:W - dw, [2, 1, 0]]
         bboxes, scores, labels, masks = seg_postprocess(
             data, bgr.shape[:2], args.conf_thres, args.iou_thres)
@@ -74,20 +99,56 @@ def main(args: argparse.Namespace) -> None:
                         cv2.FONT_HERSHEY_SIMPLEX,
                         0.75, [225, 255, 255],
                         thickness=2)
+        end_postprocessing_time = time.time()
+        postprocessing_times.append(end_postprocessing_time - start_postprocessing_time)
+        
+        # save image
+        start_saving_time = time.time()
         if args.show:
             cv2.imshow('result', draw)
             cv2.waitKey(0)
         else:
             cv2.imwrite(str(save_image), draw)
+        end_saving_time = time.time()
+        saving_times.append(end_saving_time - start_saving_time)
             
         frame_count += 1
         loop_end_time = time.time()
+        loop_times.append(loop_end_time - loop_start_time)
         loop_fps = 1.0 / (loop_end_time - loop_start_time)
         print(f"Loop FPS: {loop_fps:.2f}")
     
     current_time = time.time()
     fps = frame_count / (current_time - start_time)
     print(f"Total FPS: {fps:.2f}")
+    
+    # count avg time
+    avg_loading_time = np.array(loading_times).mean()
+    avg_preprocessing_time = np.array(preprocessing_times).mean()
+    avg_inference_time = np.array(inference_times).mean()
+    avg_postprocessing_time = np.array(postprocessing_times).mean()
+    avg_saving_time = np.array(saving_times).mean()
+    avg_loop_time = np.array(loop_times).mean()
+
+    print(f"Average Loading Time: {avg_loading_time:.3f} seconds")
+    print(f"Average Preprocessing Time: {avg_preprocessing_time:.3f} seconds")
+    print(f"Average Inference Time: {avg_inference_time:.3f} seconds")
+    print(f"Average Postprocessing Time: {avg_postprocessing_time:.3f} seconds")
+    print(f"Average Saving Time: {avg_saving_time:.3f} seconds")
+    print(f"Average Loop Time: {avg_loop_time:.3f} seconds")
+
+    # use matplotlib plot all times in one plot, using different color
+    plt.plot(loading_times, label='Loading Time', color='blue')
+    plt.plot(preprocessing_times, label='Preprocessing Time', color='orange')
+    plt.plot(inference_times, label='Inference Time', color='green')
+    plt.plot(postprocessing_times, label='Postprocessing Time', color='red')
+    plt.plot(saving_times, label='Saving Time', color='purple')
+    plt.plot(loop_times, label='Loop Time', color='olive')
+    plt.title('Time per Image for Each Step')
+    plt.xlabel('Image Index')
+    plt.ylabel('Time (seconds)')
+    plt.legend()
+    plt.savefig("time_consume_seg_without_torch.png", dpi=300)
 
 
 def parse_args():
--- infer-seg.py
+++ infer-seg.py
@@ -1,6 +1,7 @@
 from models import TRTModule  # isort:skip
 import argparse
 from pathlib import Path
+import matplotlib.pyplot as plt
 
 import cv2
 import numpy as np
@@ -29,11 +30,27 @@ def main(args: argparse.Namespace) -> None:
     frame_count = 0
     start_time = time.time()
 
+    # init times
+    loading_times = []
+    preprocessing_times = []
+    inference_times = []
+    postprocessing_times = []
+    saving_times = []
+    loop_times = []
+
     for image in images:
         loop_start_time = time.time()
         
         save_image = save_path / image.name
+        
+        # load image
+        start_loading_time = time.time()
         bgr = cv2.imread(str(image))
+        end_loading_time = time.time()
+        loading_times.append(end_loading_time - start_loading_time)
+        
+        # preprocessing
+        start_preprocessing_time = time.time()
         draw = bgr.copy()
         bgr, ratio, dwdh = letterbox(bgr, (W, H))
         dw, dh = int(dwdh[0]), int(dwdh[1])
@@ -41,9 +58,18 @@ def main(args: argparse.Namespace) -> None:
         tensor, seg_img = blob(rgb, return_seg=True)
         dwdh = torch.asarray(dwdh * 2, dtype=torch.float32, device=device)
         tensor = torch.asarray(tensor, device=device)
-        # inference
-        data = Engine(tensor)
+        end_preprocessing_time = time.time()
+        preprocessing_times.append(end_preprocessing_time - start_preprocessing_time)
 
+        
+        # inference
+        start_inference_time = time.time()
+        data = Engine(tensor)
+        end_inference_time = time.time()
+        inference_times.append(end_inference_time - start_inference_time)
+
+        # postprocessing
+        start_postprocessing_time = time.time()
         seg_img = torch.asarray(seg_img[dh:H - dh, dw:W - dw, [2, 1, 0]],
                                 device=device)
         bboxes, scores, labels, masks = seg_postprocess(
@@ -61,8 +87,7 @@ def main(args: argparse.Namespace) -> None:
         mcs = (mask_colors * inv_alph_masks).sum(0) * 2
         seg_img = (seg_img * inv_alph_masks[-1] + mcs) * 255
         draw = cv2.resize(seg_img.cpu().numpy().astype(np.uint8),
-                          draw.shape[:2][::-1])
-
+                    draw.shape[:2][::-1])
         bboxes -= dwdh
         bboxes /= ratio
 
@@ -77,20 +102,57 @@ def main(args: argparse.Namespace) -> None:
                         cv2.FONT_HERSHEY_SIMPLEX,
                         0.75, [225, 255, 255],
                         thickness=2)
+        end_postprocessing_time = time.time()
+        postprocessing_times.append(end_postprocessing_time - start_postprocessing_time)
+        
+        # save image
+        start_saving_time = time.time()
         if args.show:
             cv2.imshow('result', draw)
             cv2.waitKey(0)
         else:
             cv2.imwrite(str(save_image), draw)
+        end_saving_time = time.time()
+        saving_times.append(end_saving_time - start_saving_time)
             
         frame_count += 1
         loop_end_time = time.time()
+        loop_times.append(loop_end_time - loop_start_time)
         loop_fps = 1.0 / (loop_end_time - loop_start_time)
         print(f"Loop FPS: {loop_fps:.2f}")
     
     current_time = time.time()
+    
     fps = frame_count / (current_time - start_time)
     print(f"Total FPS: {fps:.2f}")
+    
+    # count avg time
+    avg_loading_time = np.array(loading_times).mean()
+    avg_preprocessing_time = np.array(preprocessing_times).mean()
+    avg_inference_time = np.array(inference_times).mean()
+    avg_postprocessing_time = np.array(postprocessing_times).mean()
+    avg_saving_time = np.array(saving_times).mean()
+    avg_loop_time = np.array(loop_times).mean()
+
+    print(f"Average Loading Time: {avg_loading_time:.3f} seconds")
+    print(f"Average Preprocessing Time: {avg_preprocessing_time:.3f} seconds")
+    print(f"Average Inference Time: {avg_inference_time:.3f} seconds")
+    print(f"Average Postprocessing Time: {avg_postprocessing_time:.3f} seconds")
+    print(f"Average Saving Time: {avg_saving_time:.3f} seconds")
+    print(f"Average Loop Time: {avg_loop_time:.3f} seconds")
+    
+    # use matplotlib plot all times in one plot, using different color
+    plt.plot(loading_times, label='Loading Time', color='blue')
+    plt.plot(preprocessing_times, label='Preprocessing Time', color='orange')
+    plt.plot(inference_times, label='Inference Time', color='green')
+    plt.plot(postprocessing_times, label='Postprocessing Time', color='red')
+    plt.plot(saving_times, label='Saving Time', color='purple')
+    plt.plot(loop_times, label='Loop Time', color='olive')
+    plt.title('Time per Image for Each Step')
+    plt.xlabel('Image Index')
+    plt.ylabel('Time (seconds)')
+    plt.legend()
+    plt.savefig("time_consume_seg_with_torch.png", dpi=300)
 
 
 def parse_args() -> argparse.Namespace:
